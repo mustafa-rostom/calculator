fcd70ca66d07adcd5c2d6cd1798c1ade61aaa2b4
Mustafa Rostom <mutafarostom@gmail.com>
2022-08-18 20:49:40 +0200

    Calculator logic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 import './styles.css'
+import { useReducer } from 'react';
+import DigitButton from './DigitButton';
+
+export const ACTIONS={
+  ADD_DIGIT: 'add-digit',
+  CHOOOSE_OPERATION: 'choose-opoeration',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+function reducer(state,{type,payload}){
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return{
+        ...state,
+        currentOperand: `${currentOperand || ""}${payload.digit}`
+      }
+  }
+}
 
 function App() {
+  const [{currentOperand, previousOperand,operation},dispatch]=useReducer(reducer,{})
+  dispatch({type:ACTIONS.ADD_DIGIT,payload:{digit:1}})
   return (
     <div className='calculator-grid'>
       <div className='output'>
-        <div className='previous-operand'>123,374 *</div>
-        <div className='current-operand'>123.365</div>
+        <div className='previous-operand'>{previousOperand} {operation}</div>
+        <div className='current-operand'>{currentOperand}</div>
       </div>
       <button className='span-two'>AC</button>
       <button>DEL</button>
-      <button>/</button>
+      <DigitButton digit='/' dispatch={dispatch}></DigitButton>
       <button>1</button>
       <button>2</button>
       <button>3</button>
